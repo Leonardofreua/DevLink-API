47fa1eb71bbd923a39221f3c00135f4b0964a174
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-03-15 22:51:53 -0300

    Applying bcryptjs in password.

--- api/src/app/models/Dev.js
+++ api/src/app/models/Dev.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const SALT_WORK_FACTOR = 8;
 
 const DevSchema = new mongoose.Schema(
   {
     name: String,
-    email: String,
+    email: {
+      type: String,
+      unique: true,
+    },
     password: String,
     github_username: String,
     bio: String,
@@ -18,5 +24,20 @@ const DevSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+DevSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+DevSchema.methods.validatePassword = async function validatePassword(password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model('Dev', DevSchema);
