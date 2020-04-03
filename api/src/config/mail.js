ae0dd8e8155d2dda35073d76a92120f21e0e962d
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-03 11:25:07 -0300

    Nodemailer configured.

---
+++ api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cbb4520192ac67',
+    pass: '04cc1cb5b06dac',
+  },
+  default: {
+    from: 'DevLink Team <noreply@devlinksupport.com>',
+  },
+};
