26cd9dbbccf1eec42699664dd8e25a79c308f963
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-15 20:58:44 -0300

    Changed the Header height.

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -8,23 +8,24 @@ export const Nav = styled.header`
 
   img {
     width: 120px;
+    height: 60px;
     margin-left: 60px;
   }
 `;
 
 export const Options = styled.div`
-  margin-right: 50px;
+  margin-right: 60px;
   justify-content: space-between;
 
   a:first-of-type {
-    padding: 10px;
+    padding: 7px;
     border-right: 1px solid #666;
   }
 
   a {
     margin-right: 10px;
     align-items: center;
-    font-size: 21px;
+    font-size: 17px;
     font-weight: bold;
     color: #28a745;
     text-decoration: none;
