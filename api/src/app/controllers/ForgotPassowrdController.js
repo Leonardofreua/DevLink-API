3feced91a3acca4fc2e0bf7836e935e58f934858
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-20 18:12:16 -0300

    ForgotPasswordController validations.

--- api/src/app/controllers/ForgotPassowrdController.js
+++ api/src/app/controllers/ForgotPassowrdController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import crypto from 'crypto';
 
 import Dev from '../models/Dev';
@@ -8,43 +9,52 @@ import Queue from '../../lib/Queue';
 
 class ForgotPassowrdController {
   async edit(req, res) {
-    const { email } = req.body;
-
-    const dev = await Dev.findOne({ email });
-
-    if (!dev) {
-      return res.status(401).json({
-        error: 'This email address is not associate with any account.',
-      });
-    }
-
-    const { _id, name } = dev;
-
-    const reset_password_token = crypto.randomBytes(20).toString('hex');
-
-    const reset_password_expires = Date.now() + 3600000; // expires in an hour
-
-    await Dev.findByIdAndUpdate(
-      { _id },
-      {
-        reset_password_token,
-        reset_password_expires,
-      },
-      {
-        upsert: true,
-        new: true,
-      }
-    );
-
-    await Queue.add(ForgotPassowrdMail.key, {
-      name,
-      email,
-      reset_password_token,
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
     });
 
-    res
-      .status(200)
-      .json({ message: `A reset email has been sent to ${email}.` });
+    if (await schema.isValid(req.body)) {
+      const { email } = req.body;
+
+      const dev = await Dev.findOne({ email });
+
+      if (!dev) {
+        return res.status(401).json({
+          error: 'This email address is not associate with any account.',
+        });
+      }
+
+      const { _id, name } = dev;
+
+      const reset_password_token = crypto.randomBytes(20).toString('hex');
+
+      const reset_password_expires = Date.now() + 3600000; // expires in an hour
+
+      await Dev.findByIdAndUpdate(
+        { _id },
+        {
+          reset_password_token,
+          reset_password_expires,
+        },
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+
+      await Queue.add(ForgotPassowrdMail.key, {
+        name,
+        email,
+        reset_password_token,
+      });
+
+      res
+        .status(200)
+        .json({ message: `A reset email has been sent to ${email}.` });
+    }
+    return res.status(400).json({ error: 'Validation fails' });
   }
 
   async redirectToResetPassword(req, res) {
@@ -66,39 +76,53 @@ class ForgotPassowrdController {
   }
 
   async update(req, res) {
-    const { passwd_token } = req.query;
-
-    const dev = await Dev.findOne({
-      reset_password_token: passwd_token,
-      reset_password_expires: { $gt: Date.now() },
+    const schema = Yup.object().shape({
+      newPassword: Yup.string()
+        .min(6)
+        .required(),
+      confirmPassword: Yup.string().when('newPassword', (newPassword, field) =>
+        newPassword ? field.required().oneOf([Yup.ref('newPassword')]) : field
+      ),
     });
 
-    if (!dev) {
+    if (await schema.isValid(req.body)) {
+      const { passwd_token } = req.query;
+
+      const dev = await Dev.findOne({
+        reset_password_token: passwd_token,
+        reset_password_expires: { $gt: Date.now() },
+      });
+
+      if (!dev) {
+        return res
+          .status(401)
+          .json({ message: 'Password reset token is invalid or has expired.' });
+      }
+
+      if (req.body.newPassword === req.body.confirmPassword) {
+        const { name, email } = dev;
+
+        dev.password = req.body.newPassword;
+        dev.reset_password_token = undefined;
+        dev.reset_password_expires = undefined;
+
+        await dev.save();
+
+        await Queue.add(ConfirmPasswordMail.key, {
+          name,
+          email,
+        });
+      } else {
+        return res.status(422).json({
+          error: 'Passwords do not match',
+        });
+      }
+
       return res
-        .status(401)
-        .json({ message: 'Password reset token is invalid or has expired.' });
+        .status(200)
+        .json({ message: 'Your password has been updated.' });
     }
-
-    if (req.body.newPassword === req.body.confirmPassword) {
-      const { name, email } = dev;
-
-      dev.password = req.body.newPassword;
-      dev.reset_password_token = undefined;
-      dev.reset_password_expires = undefined;
-
-      await dev.save();
-
-      await Queue.add(ConfirmPasswordMail.key, {
-        name,
-        email,
-      });
-    } else {
-      return res.status(422).json({
-        error: 'Passwords do not match',
-      });
-    }
-
-    return res.status(200).json({ message: 'Your password has been updated.' });
+    return res.status(400).json({ error: 'Validation fails' });
   }
 }
 
