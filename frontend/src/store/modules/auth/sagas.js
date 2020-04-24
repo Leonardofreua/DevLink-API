679e6de08fc1078b8e5cc2b554ca9520aeee8d6a
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-24 16:18:35 -0300

    Displaying error toasts.

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -20,6 +21,7 @@ export function* logIn({ payload }) {
 
     history.push('/home');
   } catch (err) {
+    toast.error('Authentication failed, check your Email and Password');
     yield put(signFailure());
   }
 }
