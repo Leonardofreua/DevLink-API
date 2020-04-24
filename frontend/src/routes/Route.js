6d72b05ccd7ed326e49d2294d4a41ea62f3bbc63
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-24 16:05:14 -0300

    Persisting authentication

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layout/auth';
 import NoAuthLayout from '~/pages/_layout/noAuth';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
