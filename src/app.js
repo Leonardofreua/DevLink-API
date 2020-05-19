d1acbfbe56f5ab9a372d767bd79f06e6f19b90bc
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-05-19 19:01:56 -0300

    added CORS url to client.

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(cors());
+    this.server.use(cors('https://epic-volhard-29142a.netlify.app'));
     this.server.use(express.json());
     this.server.use(
       '/files',
